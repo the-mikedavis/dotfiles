d1c612adfde2820a0c9695fbc3dbba9535bb3938
Michael Davis <mcarsondavis@gmail.com>
2021-06-20 12:52:00 -0500

    add z and gh

--- home-manager/.config/nixpkgs/home.nix
+++ home-manager/.config/nixpkgs/home.nix
@@ -5,7 +5,26 @@
   programs.home-manager.enable = true;
 
   # fish shell
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "ddeb28a7b6a1f0ec6dae40c636e5ca4908ad160a";
+          sha256 = "0c5i7sdrsp0q3vbziqzdyqn4fmp235ax4mn4zslrswvn8g3fvdyh";
+        };
+      }
+    ];
+  };
+
+  programs.gh = {
+    enable = true;
+    editor = "kak";
+    gitProtocol = "ssh";
+  };
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
@@ -40,6 +59,8 @@
     slack
     stow
     jetbrains-mono
+    xdg-utils
+    wdisplays
   ];
 
   programs.kakoune = {
