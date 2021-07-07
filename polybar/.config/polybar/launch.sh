0bb8934e197b5e55fd778375e61ee118db338af4
Michael Davis <mcarsondavis@gmail.com>
2021-07-07 07:30:08 -0500

    remove unused configs

--- polybar/.config/polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-killall -q polybar
-
-polybar main >> /tmp/polybar-main.log 2>&1 & disown
-
-echo "Bar(s) launched!"
--- polybar/.config/polybar/vpn.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env fish
-
-if [ (nordvpn status | grep Connected) ]
-  set -l ip (nordvpn status | grep IP | sed 's/Your new IP: //')
-  set -l city (nordvpn status | grep City | sed 's/City: //')
-
-  echo ""$ip"@"$city
-end
