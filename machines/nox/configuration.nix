b986725a10ed04def4b64cab51377d7b3d3daf5e
Michael Davis <mcarsondavis@gmail.com>
2021-09-15 09:15:10 -0500

    setup home configuration within the system configuration for nox

--- machines/nox/configuration.nix
+++ machines/nox/configuration.nix
@@ -1,19 +1,40 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 let
+  unstable = import <nixos-unstable> { };
   dirs = {
     defaults = ../../defaults;
+    colorschemes = ../../colorschemes;
+  };
+  home-manager = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz";
+  };
+  configs = {
+    fish = import (dirs.defaults + /fish) { inherit pkgs; };
+    ssh = import (dirs.defaults + /ssh);
+    kakoune = import (dirs.defaults + /kakoune) { inherit pkgs; };
+    git = import (dirs.defaults + /git);
+    sway = import (dirs.defaults + /sway) { inherit pkgs; };
+    kitty = import (dirs.defaults + /kitty);
+    firefox = import (dirs.defaults + /firefox) { inherit pkgs; };
+    gpg = import (dirs.defaults + /gpg);
+    gtk = import (dirs.defaults + /gtk) { inherit pkgs; };
+    fzf = import (dirs.defaults + /fzf);
   };
 in
 {
   imports =
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
+      "${home-manager}/nixos"
     ];
+
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -207,6 +228,126 @@ in
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+
+  home-manager.users.michael = { pkgs, ... }: {
+    programs.home-manager.enable = true;
+
+    # fish shell
+    programs.fish = {
+      enable = true;
+    } // configs.fish;
+
+    programs.gh = {
+      enable = true;
+      editor = "kak";
+      gitProtocol = "ssh";
+    };
+
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    home.username = "michael";
+    home.homeDirectory = "/home/michael";
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "21.05";
+
+    programs.exa.enable = true;
+    programs.command-not-found.enable = true;
+
+    home.packages = with pkgs; [
+      tree
+      curl
+      neofetch
+      docker-compose
+      docker-sync
+      dnsutils
+      traceroute
+      nmap
+      ipcalc
+      discord
+      slack
+      stow
+      jetbrains-mono
+      xdg-utils
+      swaylock
+      swayidle
+      wl-clipboard
+      grim
+      slurp
+      wf-recorder
+      wdisplays
+      wofi
+      chromium
+      file
+      aspell
+      aspellDicts.en
+      git-crypt
+      subsurface
+      ripgrep
+      imv
+      # unstable.helix
+    ];
+
+    home.file.".aspell.conf".text = "data-dir ${pkgs.aspell}/lib/aspell";
+
+    programs.ssh = {
+      enable = true;
+    } // configs.ssh;
+
+    programs.kakoune = {
+      enable = true;
+    } // configs.kakoune;
+
+    xdg.configFile."kak/colors/grv.kak".source = (dirs.colorschemes + /kakoune/grv.kak);
+
+    programs.git = {
+      enable = true;
+    } // configs.git;
+
+    programs.waybar = {
+      enable = true;
+    };
+    # taking manual control of the waybar config since nix tells me the config
+    # is wrong
+    xdg.configFile."waybar/config".source = (dirs.defaults + /waybar/config.json);
+    xdg.configFile."waybar/style.css".source = (dirs.defaults + /waybar/style.css);
+
+    wayland.windowManager.sway = {
+      enable = true;
+    } // configs.sway;
+
+    programs.kitty = {
+      enable = true;
+    } // configs.kitty;
+
+    programs.firefox = {
+      enable = true;
+    } // configs.firefox;
+
+    services.gpg-agent = {
+      enable = true;
+    } // configs.gpg;
+
+    gtk = {
+      enable = true;
+    } // configs.gtk;
+
+    programs.fzf = {
+      enable = true;
+    } // configs.fzf;
+
+    nixpkgs.config = {
+      allowUnfree = true;
+      chromium.enableWideVine = true;
+    };
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
