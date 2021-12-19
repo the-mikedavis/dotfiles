a8f8aa02e28f98351c01ccaee005ac3b1cb4009c
Michael Davis <mcarsondavis@gmail.com>
2021-12-18 22:07:46 -0600

    remove unstable tree-sitter

--- flake.nix
+++ flake.nix
@@ -39,7 +39,6 @@
             pkgs-unstable._1password-gui
             pkgs-unstable.discord
             pkgs-unstable.spotify
-            (pkgs-unstable.tree-sitter.override { webUISupport = true; })
             pkgs-unstable.element-desktop
             pkgs-unstable.wireshark
             inputs.kak-buffercraft.defaultPackage.${system}
