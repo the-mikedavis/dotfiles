95f652fb19785c9b7ff26ce2d8f355b6099d1c32
Michael Davis <mcarsondavis@gmail.com>
2022-01-07 07:15:35 -0600

    add brightnessctl to nox

--- machines/nox/configuration.nix
+++ machines/nox/configuration.nix
@@ -39,6 +39,8 @@ in
     '';
   };
 
+  environment.systemPackages = with pkgs; [ brightnessctl ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
