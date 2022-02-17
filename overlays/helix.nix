4767713774fb87b369725585719c75c334ca92a0
Michael Davis <mcarsondavis@gmail.com>
2022-02-16 18:56:55 -0600

    compile grammars in nix derivation

--- overlays/helix.nix
+++ overlays/helix.nix
@@ -1,23 +1,43 @@
 { fetchFromGitHub, lib, rustPlatform, makeWrapper }:
 
 # a derivation of helix based on my fork
-rustPlatform.buildRustPackage rec {
-  pname = "helix";
-  version = "0.6.0.1-tmd";
-
+let
   src = fetchFromGitHub {
     owner = "the-mikedavis";
-    repo = pname;
-    rev = "23a37037a23222e6fa990ba0c9d5537a9d1e7c80";
+    repo = "helix";
+    rev = "4da7aa0441be8dd7df775cdf3c39be78f7cc7ccd";
 
     # when building from a new rev, this value clashes first with lib.fakeSha256, then the
     # cargoSha256 after that
     #
-    sha256 = "sha256-2loAsmSAZzadWGUTGu/GQvEMwPP4+YzYMYJQ1voYxX4=";
+    sha256 = "sha256-o021YCSXiGwZH9qZVLICumZhd8tfG5Kry4GWkME7QiU=";
     # sha256 = lib.fakeSha256;
   };
+  config = (builtins.fromTOML
+    (builtins.readFile "${builtins.toPath src}/languages.toml")).language;
+  gitGrammars = builtins.filter (lang:
+    builtins.hasAttr "grammar" lang && builtins.hasAttr "source" lang.grammar
+    && builtins.hasAttr "git" lang.grammar.source) config;
+  grammars = builtins.map (lang: {
+    name = lang.name;
+    source = fetchGit {
+      url = lang.grammar.source.git;
+      rev = lang.grammar.source.rev;
+      allRefs = true;
+    };
+  }) gitGrammars;
+  grammarLinks = builtins.foldl' (acc: grammar: ''
+    ln -s ${
+      builtins.toPath grammar.source
+    } $out/lib/runtime/grammars/sources/${grammar.name}
+    ${acc}'') "\n" grammars;
+in rustPlatform.buildRustPackage rec {
+  inherit src;
 
-  cargoSha256 = "sha256-y7bciYVv2bwvLr5UUcOIgh/qcQmv/ZSgakTmOmVFG5Q=";
+  pname = "helix";
+  version = "0.6.0.3-tmd";
+
+  cargoSha256 = "sha256-TzMGITvJ3vWiwtyRu+s1J7v88QskTx1NV1W5yA9Bgww=";
   # cargoSha256 = lib.fakeSha256;
 
   nativeBuildInputs = [ makeWrapper ];
@@ -25,17 +45,23 @@ rustPlatform.buildRustPackage rec {
   postInstall = ''
     mkdir -p $out/lib
     cp -r runtime $out/lib
+    mkdir -p $out/lib/runtime/grammars/sources
+
+    ${grammarLinks}
+
+    HELIX_RUNTIME=$out/lib/runtime $out/bin/hx --build-grammars
   '';
+
   postFixup = ''
     wrapProgram $out/bin/hx --set HELIX_RUNTIME $out/lib/runtime
   '';
 
-  # HAXXX -
-  # Turn off tests: we need at least the rust grammar built to run tests
-  # successfully after #1659. I should be able to fix this within my
-  # own dotfiles because I use nix 2.6, but this is not easy to fix in
-  # nix < 2.6 because `builtins.fromTOML` has bug that fails to parse
-  # languages.toml.
+  # HACK:
+  # Turn off tests - we need at least the rust grammar built to run tests
+  # successfully after #1659. This could be fixed in theory: nix can build
+  # the grammars just fine itself, and that might even make the postInstall
+  # step much cleaner. So just build the grammars in a separate derivation
+  # and then drop them in in the postUnpack phase.
   doCheck = false;
 
   meta = with lib; {
