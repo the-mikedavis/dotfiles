dac982eb17a1b32169bf9b0da0f5520b2ab5546b
Michael Davis <mcarsondavis@gmail.com>
2022-02-18 08:16:35 -0600

    use new dirty branch for helix with [[grammar]] refactor

--- overlays/helix.nix
+++ overlays/helix.nix
@@ -5,39 +5,40 @@ let
   src = fetchFromGitHub {
     owner = "the-mikedavis";
     repo = "helix";
-    rev = "e8d81b0b617a556001f04cc0b38ae279d7385094";
+    rev = "56b13f148816e3e48a664fb5a15ce953436584cf";
 
     # when building from a new rev, this value clashes first with lib.fakeSha256, then the
     # cargoSha256 after that
     #
-    sha256 = "sha256-R73jc7YGPmc2DU73ET1biVBf3U7fhn8KldmPy1GE4To=";
+    sha256 = "sha256-fcHixVnHOx6q8DsyGgyaUE/4G9xlbMKLLLnFxOn/fgU=";
     # sha256 = lib.fakeSha256;
   };
-  config = (builtins.fromTOML
-    (builtins.readFile "${builtins.toPath src}/languages.toml")).language;
-  gitGrammars = builtins.filter (lang:
-    builtins.hasAttr "grammar" lang && builtins.hasAttr "source" lang.grammar
-    && builtins.hasAttr "git" lang.grammar.source) config;
-  grammars = builtins.map (lang: {
-    name = lang.name;
+
+  config = builtins.fromTOML (builtins.readFile "${src}/languages.toml");
+  isGitGrammar = (grammar:
+    builtins.hasAttr "source" grammar && builtins.hasAttr "git" grammar.source
+    && builtins.hasAttr "rev" grammar.source);
+  gitGrammars = builtins.filter isGitGrammar config.grammar;
+  mapGrammar = grammar: {
+    name = grammar.name;
     source = fetchGit {
-      url = lang.grammar.source.git;
-      rev = lang.grammar.source.rev;
+      url = grammar.source.git;
+      rev = grammar.source.rev;
       allRefs = true;
     };
-  }) gitGrammars;
+  };
+  grammars = builtins.map mapGrammar gitGrammars;
+
   grammarLinks = builtins.foldl' (acc: grammar: ''
-    ln -s ${
-      builtins.toPath grammar.source
-    } $out/lib/runtime/grammars/sources/${grammar.name}
+    ln -s ${grammar.source} $out/lib/runtime/grammars/sources/${grammar.name}
     ${acc}'') "\n" grammars;
 in rustPlatform.buildRustPackage rec {
   inherit src;
 
   pname = "helix";
-  version = "0.6.0.3-tmd";
+  version = "0.6.0.4-tmd";
 
-  cargoSha256 = "sha256-TzMGITvJ3vWiwtyRu+s1J7v88QskTx1NV1W5yA9Bgww=";
+  cargoSha256 = "sha256-VOLm7/hmNUX21skw8hp9ep9svenqq5D0MVxwctKihts=";
   # cargoSha256 = lib.fakeSha256;
 
   nativeBuildInputs = [ makeWrapper ];
