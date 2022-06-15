d002b97a242b1adbf50c767c3a43d210afb6c630
Michael Davis <mcarsondavis@gmail.com>
2022-06-15 12:52:47 -0500

    unpersist root's nix trusted settings
    The rebuild-switch can't read the link, it appears to be broken.
    Not sure how that got broken but for now I'm ok hitting `y<ret>`
    twice on the initial rebuild-switch.

--- users/root.nix
+++ users/root.nix
@@ -8,9 +8,5 @@ impermanence: { pkgs, ... }:
   };
 
   home.persistence."/nix/persist/home/root" = {
-    files = [
-      # trusted settings in nix like `nix-command` and `flakes`
-      ".local/share/nix/trusted-settings.json"
-    ];
   };
 }
