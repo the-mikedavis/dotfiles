5b7ba9dff0c9239e587d712a2d8fa6256504fcba
Michael Davis <mcarsondavis@gmail.com>
2022-06-19 21:18:22 -0500

    lazygit: add C-p binding that checks out a PR by number

--- users/michael.nix
+++ users/michael.nix
@@ -282,6 +282,22 @@ in
     enable = true;
     settings = {
       promptToReturnFromSubprocess = false;
+      customCommands = [
+        # C-p checks out a pull request by number
+        # https://github.com/jesseduffield/lazygit/wiki/Custom-Commands-Compendium#checkout-branch-via-github-pull-request-id
+        {
+          key = "<c-p>";
+          prompts = [
+            {
+              type = "input";
+              title = "PR #";
+            }
+          ];
+          command = "gh pr checkout {{ index .PromptResponses 0 }}";
+          context = "localBranches";
+          loadingText = "Checking out PR";
+        }
+      ];
     };
   };
 
