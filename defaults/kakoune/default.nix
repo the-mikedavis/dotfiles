b4e648f11c3da8d8c6fa75e8b158b125b5651104
Michael Davis <mcarsondavis@gmail.com>
2022-12-16 16:29:15 -0600

    Make Kakoune more stock-like
    Custom keymaps just end up being confusing since I don't use this
    nearly as much. I actually thought this was the default keymap 🙃

--- defaults/kakoune/default.nix
+++ defaults/kakoune/default.nix
@@ -10,19 +10,5 @@
   extraConfig = ''
     # tune down the start-up info thingy
     set-option global startup_info_version 20210923
-
-    # make x extend the selection down, X extend up
-    def -params 1 extend-line-down %{
-      exec "<a-:>%arg{1}X"
-    }
-
-    def -params 1 extend-line-up %{
-      exec "<a-:><a-;>%arg{1}K<a-;>"
-      try %{
-        exec -draft ';<a-K>\n<ret>'
-        exec X
-      }
-      exec '<a-;><a-X>'
-    }
   '';
 }
