b543ff60c86e75da2ed248146260a715bf3019c8
Michael Davis <mcarsondavis@gmail.com>
2023-04-16 09:20:28 -0500

    kakoune: Setup kak-lsp in config

--- defaults/kakoune/default.nix
+++ defaults/kakoune/default.nix
@@ -10,5 +10,20 @@
   extraConfig = ''
     # tune down the start-up info thingy
     set-option global startup_info_version 20210923
+
+    # plugins
+    evaluate-commands %sh{
+      plugins="$kak_config/plugins"
+      mkdir -p "$plugins"
+      [ ! -e "$plugins/plug.kak" ] && \
+        git clone -q https://github.com/andreyorst/plug.kak.git "$plugins/plug.kak"
+      printf "%s\n" "source '$plugins/plug.kak/rc/plug.kak'"
+    }
+    plug "andreyorst/plug.kak" noload
+    plug "kak-lsp/kak-lsp" config %{
+      # config here
+      map global insert <tab> '<a-;>:try lsp-snippets-select-next-placeholders catch %{ execute-keys -with-hooks <lt>tab> }<ret>' -docstring 'Select next snippet placeholder'
+
+    }
   '';
 }
